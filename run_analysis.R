8724481df0a5e2323451f1918f4e0bd1d6e00aed
cprabucki <carlos.von@atos.net>
2017-02-19 20:58:00 +0100

    Getting and Cleaning Data Course Project v4

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@
 library(dplyr)
 
 # Sets the working directory (only for my use)
-#setwd("C:\\Users\\a212857\\Documents\\GitHub\\ProgrammingAssignment4.Cleaning\\wd")
+setwd("C:\\Users\\a212857\\Documents\\GitHub\\ProgrammingAssignment4.Cleaning\\wd")
 
 ## ******************************************************************
 ## 1.  Merges the training and the test sets to create one data set
@@ -13,12 +13,13 @@ library(dplyr)
 test   <-read.table("X_test.txt",   sep='')
 train  <-read.table("X_train.txt",  sep='')
 
-# Merges the 2 files of observations and free up train and test
+# Merges the 2 files of observations and free up the temporary train and test data frames
 total <- rbind(test, train)
 ls(test, train)
 
 ## *******************************************************************************************
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+##      Please note that any variable containing 'mean' or 'std' will be selected
 ## *******************************************************************************************
 
 # Load the file into the DF
@@ -27,28 +28,32 @@ features <-read.table("features.txt", sep="")
 # Identify wich features are linked to "mean" or "std" and keep both the index and the description
 features <- features [grepl("(-mean|-std)", features[,2]), ]
 
-# Extracts only the desired measurements
+# Extracts only the desired measurements while features[,2] holds the descriptive names of the variables
 total <- select(total, features[,1])
 
 ## ***************************************************************************
 ## 3. Uses descriptive activity names to name the activities in the data set
 ## ***************************************************************************
 
-#Retrieves the Activities from the 'y_test.txt' and 'y_train.txt' files and merges them in a single vector
+# Retrieves the Activities from the 'y_test.txt' and 'y_train.txt' files and merges them in a single vector
 ytest   <-read.table("y_test.txt",   sep='')
 ytrain  <-read.table("y_train.txt",  sep='')
 ytotal <- rbind(ytest, ytrain)
+
+## Frees up the temporary datasets ytest and ytrain
 ls(ytest, ytrain)
+
+# Asign the name 'activity_indez' to the merged vector
 names(ytotal) <- c("activity_index")
 
-# Adds the column to the main dataset 'total' as lasts column 
+# Adds the column to the main dataset 'total' as last column 
 total <-cbind(total, ytotal)
 
 # For each activity index we find out the right activity label 
 activ <- read.table("activity_labels.txt", sep="")
 names(activ) <- c("activity_index", "activity")
 
-# Before we merge the activity (loosing original order) we include the subject column for point 5)
+# Before we merge the activity (and therefore loosing original order) we include the subject column for point 5)
 stest   <-read.table("subject_test.txt",  sep='')
 strain  <-read.table("subject_train.txt", sep='')
 stotal <- rbind(stest, strain)
@@ -57,15 +62,17 @@ names(stotal) <- c("subject")
 total <-cbind(total, stotal)
 
 
-# Finally we merge the main dataset and the activities labels. Beware that from this point on the original order is lost
+# Finally we merge the main dataset and the activities labels.
+# Beware that from this point on the original order is lost
 total <- merge(total, activ)
 
 ## ***************************************************************************
 ## 4. Appropriately labels the data set with descriptive variable names
 ## ***************************************************************************
 
-# Assign the right measurement names taking them from the 'features' selection coming from point 2)
-names(total) <- c("activity_index", as.character(features[,2]), "subject", "activity")
+# Orders and assign the right measurement names taking them from the 'features' selection coming from point 2)
+total <- total[c(1,82, 81,2:80)]
+names(total) <- c("activity_index", "activity", "subject", as.character(features[,2]))
 
 
 ## ******************************************************************************************
@@ -83,16 +90,21 @@ summ <- function(df, i){
 }
 
 # Here we iterate on the activities through the index and append the resulting summaries into the new data frame
-lrefin <- data.frame()
-for (i in 1:6){
-        lrefin <- rbind(lrefin, summ(total, i))
-}
+tidy <- data.frame()
+for (i in 1:6) tidy <- rbind(tidy, summ(total, i))
+
+# Adds to the tidy data set the activity labels (vlookup-like)
+tidy <- merge(tidy, activ, by="activity_index")
+
+# Reorders the columns and eliminate activity index
+tidy <- tidy[c(1, 82, 2, 3:81)]
+tidy <- select(tidy, -activity_index)
 
 ## ******************************************************************************************
 ## 6. Writes the final tidy data frame
 ## ******************************************************************************************
 
 
-# Write the file as requested
-write.table(lrefin, "Final_Tidy.txt", row.name=FALSE)
+# Write the tidy final result in a file as requested
+write.table(tidy, "Tidy.txt", row.name=FALSE)
 
